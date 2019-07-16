c22ad8955c4866c89fd1d8640a8899f74aa946c7
kabuhakmeh3 <kabuhakmeh@gmail.com>
2019-07-16 12:42:16 -0700

    split into nb & no-nlp labeling

--- scripts/roles_by_city_nb.py
+++ scripts/roles_by_city_nb.py
@@ -1,5 +1,8 @@
 import os, sys, time, pickle
 import pandas as pd
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import ComplementNB
 
 # add custom modules to path
 path_to_module = '/home/ubuntu/job-classifier/tools/'
@@ -18,41 +21,41 @@ def get_city_df(df, city, role):
     city_df = city_df[city_df.city.str.lower()==city]
     return city_df
 
-role_path = '/home/ubuntu/job-classifier/.keys/'
-roles = load_pickle(os.path.join(role_path, 'role_dict.pickle'))
-
-def get_role(job_title role_dict=roles):
-    '''Label each row by the type of job it is
-    '''
-    for role in role_dict:
-        if role in job_title:
-            return role_dict[role]
-    return 'ignore'
-
 def main():
     '''Run classification on job data
 
     Process directly from partner feeds
 
-    ** NOTE - this is a "dumb" method without NLP/ML **
-
     Current configuration:
 
     + Load csv data from xml feed
-    + Label based on job title
-    + Write results to s3
+    + Count Vectorize with Bag of Words
+    + Label using Naive Bayes Classifier
+    + Write results back to s3
     '''
 
     # define paths
     key_path = '/home/ubuntu/job-classifier/.keys/'
+    model_path = '/home/ubuntu/job-classifier/models/'
 
     # dict with partner:url pairs
     partners = load_pickle(os.path.join(key_path, 'partners.pickle'))
     s3_details = load_pickle(os.path.join(key_path, 's3_config.pickle'))
-    city_roles = load_pickle(os.path.join(key_path, 'city_roles.pickle'))
 
     bucket = s3_details['csv_bucket']
     file_to_write = s3_details['target']
+
+    cv_model = load_pickle(os.path.join(model_path,'CV_nb_bow_model.pckl'))
+    # update cv model above
+    #mnb_model = load_pickle(os.path.join(model_path, 'multi_nb_model.pckl'))
+    cnb_model = load_pickle(os.path.join(model_path, 'complement_nb_model.pckl'))
+
+    # cities & roles
+    city_roles = {
+            'driver' : ['new york','los angeles'],
+            'nurse' : ['dallas', 'los angeles'],
+            'tech' : ['san francisco', 'boston']
+            }
 
     # pull xml from url and parse into df
     for partner in partners:
@@ -66,29 +69,38 @@ def main():
         # standardize text format
         df = nlp.standardize_text(df, 'title')
 
+        # select data to predict from
+        X_classify = df['title'].tolist()
 
+        # get count vec
+        X_classify_counts = nlp.get_cv_test_counts(X_classify, cv_model)
+
+        # predict with model
+        #y_label = mnb_model.predict(X_classify_counts)
         y_label = cnb_model.predict(X_classify_counts)
 
-        # assign labels to jobs & prune dataframe
-        df['label'] = df.title.apply(get_role)
+        # assign predictions to jobs & prune dataframe
+        df['label'] = y_label
 
         label_cols = ['label', 'company','title','city','state','url']
-
-        df = df[~(df['label']=='ignore')][label_cols]
-
+        #labels_to_drop = ['ignore','driver','service']
+        labels_to_drop = ['ignore','service']
+        
+        df_tmp = df[~(df['label'].isin(labels_to_drop))][label_cols]
+        
         for role in city_roles:
             print('processing:', role.upper())
-
+            
             for city in city_roles[role]:
                 print('city:', city.upper())
-
+                
                 df_to_write = get_city_df(df, city, role)
-
+        
                 # write labeled roles
                 city_file = city.replace(' ', '_')
                 label_key = partner + '/' + role + '/' + city_file + '/' + 'jobs.csv'
                 bt.write_df_to_s3(df_to_write, bucket, label_key, comp=False)
-
+            
 
 if __name__ == '__main__':
     main()
